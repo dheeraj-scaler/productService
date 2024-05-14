35d1f02a77d0daa13155deb8bbc86282fd29f39b
Default User <defaultuser@Defaults-MacBook-Air.local>
2024-05-14 20:17:51 +0530

    First changes

---
+++ src/main/java/com/scaler/demoproject/DemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.demoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/demoproject/DemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.demoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
